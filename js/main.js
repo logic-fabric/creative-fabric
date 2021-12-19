7290ec5189cbbd10dcc9622c79a2e6282b07a7fb
logic-fabric <lowic@logic-fabric.fr>
2021-12-19 17:24:46 +0100

    feature: return focus to the last active element after closing the dialog

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 const dialog = document.querySelector("dialog");
-dialog.showModal();
+
+function openDialog() {
+  dialog.showModal();
+}
+
+function closeDialog() {
+  dialog.close();
+  
+  // Returns focus to the last active element:
+  openDialogBtn.focus();
+}
+
+const openDialogBtn = document.getElementById("open-dialog-btn");
+const closeDialogBtn = document.getElementById("close-dialog-btn");
+
+openDialogBtn.addEventListener("click", openDialog);
+closeDialogBtn.addEventListener("click", closeDialog);
