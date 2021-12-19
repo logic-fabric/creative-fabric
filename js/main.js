83ec7b235a2faec46747dbcb265bcf5d169d39d0
logic-fabric <lowic@logic-fabric.fr>
2021-12-19 17:42:00 +0100

    feature: trap focus inside the dialog while open

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 const dialog = document.querySelector("dialog");
 
+const dialogInteractiveElements = dialog.querySelectorAll(
+  "a, button, input, textarea, select, details, [tabindex]:not([tabindex='-1'])"
+);
+const firstInteractiveElement = dialogInteractiveElements[0];
+const lastInteractiveElement =
+  dialogInteractiveElements[dialogInteractiveElements.length - 1];
+
+function trapFocus(event) {
+  if (event.key === "Tab") {
+    const tabForwards =
+      !event.shiftKey && document.activeElement === lastInteractiveElement;
+    const tabBackwards =
+      event.shiftKey && document.activeElement === firstInteractiveElement;
+
+    if (tabForwards) {
+      event.preventDefault();
+      firstInteractiveElement.focus();
+    } else if (tabBackwards) {
+      event.preventDefault();
+      lastInteractiveElement.focus();
+    }
+  }
+}
+
 function openDialog() {
   dialog.showModal();
+  dialog.addEventListener("keydown", trapFocus);
 }
 
 function closeDialog() {
   dialog.close();
-  
+  dialog.removeEventListener("keydown", trapFocus);
+
   // Returns focus to the last active element:
   openDialogBtn.focus();
 }
